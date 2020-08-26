6d4600757194d996eae756a73e281c4e43d21716
weverton <wevertonprocopio690@gmail.com>
2020-08-26 08:20:37 -0300

    imgs alinhadas e add icon

--- js/styles.js
+++ js/styles.js
@@ -1,26 +1,21 @@
 function trocaDeCapa(hora, minuto, segundos) {
-  const capa = document.querySelector('#capa')
+  const capa = document.querySelector("#capa");
   if (hora >= 6 && hora <= 11) {
-    capa.setAttribute('src', 'imagens/capa/capa1.jpg')
+    capa.setAttribute("src", "imagens/capa/capa1.jpg");
+  } else if (hora >= 12 && hora <= 17) {
+    capa.setAttribute("src", "imagens/capa/download.jpeg");
+  } else if (hora >= 18 && hora <= 23) {
+    capa.setAttribute("src", "imagens/capa/capa3.jpg");
+  } else {
+    capa.setAttribute("src", "imagens/capa/capa4.jpg");
   }
-  else if (hora >= 12 && hora <= 17) {
-    capa.setAttribute('src', 'imagens/capa/download.jpeg')
-  }
-  else if (hora >= 18 && hora <= 23) {
-    capa.setAttribute('src', 'imagens/capa/capa3.jpg')
-  }
-  else {
-    capa.setAttribute('src', 'imagens/capa/capa4.jpg')
-  }
-  let res = document.querySelector('#res')
-  res.innerHTML = ` Não está na hora de jogar Free Fire ? ${hora}: ${minuto}: ${segundos}`
+  let res = document.querySelector("#res");
+  res.innerHTML = ` Não está na hora de jogar Free Fire ? ${hora}: ${minuto}: ${segundos}`;
 }
 setInterval(() => {
-  let day = new Date()
-  let hr = day.getHours()
-  let min = day.getMinutes()
-  let sgds = day.getSeconds()
-  trocaDeCapa(hr, min, sgds)
-}, 1000)
-
-trocaDeCapa()
+  let day = new Date();
+  let hr = day.getHours();
+  let min = day.getMinutes();
+  let sgds = day.getSeconds();
+  trocaDeCapa(hr, min, sgds);
+}, 1000);
